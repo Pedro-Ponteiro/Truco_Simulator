e5301130f6936e3a806cfc93a9df4d126203d03b
pedro-ponteiro <silv9797@gmail.com>
2022-01-09 16:45:41 -0300

    [feat] adc opção de o usuario escolher a quantidade de maos para gerar

--- Start.py
+++ Start.py
@@ -4,8 +4,11 @@ import modules.gerar_dados
 
 
 def main() -> None:
+
+    nr_maos = int(input("Quantidade de maos para gerar: \n-> "))
+
     start = perf_counter()
-    qtd_maos = modules.gerar_dados.main()
+    qtd_maos = modules.gerar_dados.main(nr_maos)
     elapsed = perf_counter() - start
     print(f"Elapsed time (seconds): {elapsed:.2f}")
     print(f"Hands per second: {qtd_maos//elapsed}")
--- modules/gerar_dados.py
+++ modules/gerar_dados.py
@@ -1,16 +1,17 @@
+import os
+
 import pandas as pd
 
 from modules.classes.classes_base import Jogo, Time
 from modules.classes.jogadores import JogadorProbabilistico
 
 
-def main() -> int:
+def main(nr_maos: int) -> int:
     jogador1, jogador2 = JogadorProbabilistico("Pedro"), JogadorProbabilistico("Manu")
     jogador3, jogador4 = JogadorProbabilistico("Mari"), JogadorProbabilistico("Ariel")
     time1 = Time("PENU", jogador1, jogador2)
     time2 = Time("ARIMA", jogador3, jogador4)
 
-    nr_maos = 40_000_000
     nr_mesas = nr_maos // 4
 
     jogo = Jogo(nr_mesas, [jogador1, jogador3, jogador2, jogador4], time1, time2)
@@ -18,7 +19,7 @@ def main() -> int:
     df = pd.DataFrame(jogo.mesas_stats)
     print(f"Quantidade de mãos geradas: {len(df)}")
 
-    df.to_pickle(r".\modules\db\dados.pickle")
-    df.to_csv(r".\modules\db\dados.csv")
+    df.to_pickle(os.path.join("modules", "db", "dados.pickle"))
+    df.to_csv(os.path.join("modules", "db", "dados.csv"))
 
     return len(df)
