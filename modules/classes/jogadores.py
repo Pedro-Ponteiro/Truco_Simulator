6ad952f161ea05f24ce3c972591dbd85f021fc3c
pedro-ponteiro <silv9797@gmail.com>
2022-01-09 21:41:49 -0300

    [refactor+fix] refatorado com classe Estrategia e corrigido alguns problemas de lógica

--- modules/classes/jogadores.py
+++ modules/classes/jogadores.py
@@ -1,7 +1,112 @@
-from typing import List, Tuple
+from typing import List, Optional
 
 from modules.classes.baralho import Carta
-from modules.classes.classes_base import Jogada, Jogador, Mesa
+from modules.classes.classes_base import Jogada, Jogador, Mesa, Time
+
+
+class Estrategia:
+    def __init__(
+        self,
+        turno: int,
+        maior_carta_rodada: Optional[Jogada],
+        pontos_time_rival: int,
+        jogador: "JogadorProbabilistico",
+        jogadas_rodada: List[Jogada],
+    ) -> None:
+        self.turno = turno
+        self.maior_carta_rodada = maior_carta_rodada
+        self.pontos_time_rival = pontos_time_rival
+        self.jogador = jogador
+        self.jogadas_rodada = jogadas_rodada
+
+        self.turno_estrategia = {
+            1: self.primeiro_turno,
+            2: self.segundo_terceiro_turno,
+            3: self.segundo_terceiro_turno,
+            4: self.quarto_turno,
+        }
+
+    def escolher_melhor_carta(self) -> Carta:
+        if len(self.jogador.mao) == 1:
+            return self.jogador.get_menor_carta_mao()
+
+        return self.turno_estrategia[self.turno]()
+
+    def primeiro_turno(self) -> Carta:
+        if self.pontos_time_rival > 0:
+            # print("iniciando com a minha maior")
+            return self.jogador.get_maior_carta_mao()
+        else:
+            # print("iniciando com a minha menor")
+            return self.jogador.get_menor_carta_mao()
+
+    def segundo_terceiro_turno(self) -> Carta:
+
+        maior_carta_mao = self.jogador.get_maior_carta_mao()
+        menor_carta_mao = self.jogador.get_menor_carta_mao()
+
+        if self.pontos_time_rival == 1 and self.jogador.time.pontos_mesa == 1:
+            return maior_carta_mao
+
+        if self.is_empate():
+            if self.pontos_time_rival > 0:
+                # devo cobrir!
+                # print("Cobrindo o empate")
+                carta_jogada = maior_carta_mao
+                if carta_jogada.num <= self.maior_carta_rodada.valor_carta:
+                    carta_jogada = menor_carta_mao
+            else:
+                # print(jogadas_rodada)
+                # print("Deixo o empate acontecer")
+                carta_jogada = menor_carta_mao
+        else:
+            if self.maior_carta_rodada.jogador == self.jogador.parceiro:
+                # descarte
+                # print("Meu parceiro esta fazendo, então...")
+                carta_jogada = menor_carta_mao
+            else:
+                if self.maior_carta_rodada.valor_carta <= maior_carta_mao.num:
+                    # print("vou cobrir ou empatar essa carta com a minha maior")
+                    carta_jogada = maior_carta_mao
+                else:
+                    # print("Descarto, pois não consigo cobrir")
+                    carta_jogada = menor_carta_mao
+
+        return carta_jogada
+
+    def quarto_turno(self) -> Carta:
+
+        if self.is_empate():
+            if self.pontos_time_rival > 0:
+                return self.jogador.get_maior_carta_mao()
+            else:
+                return self.jogador.get_menor_carta_mao()
+
+        menor_carta_mao = self.jogador.get_menor_carta_mao()
+
+        if self.maior_carta_rodada.jogador == self.jogador.parceiro:
+            # descarte
+            return menor_carta_mao
+
+        valor_a_ser_alcancado = self.maior_carta_rodada.valor_carta
+        if self.pontos_time_rival == 1 and self.jogador.time.pontos_mesa != 1:
+            # tenho que me esforçar pra jogar um pouco maior
+            valor_a_ser_alcancado += 1
+
+        return self.jogador.escolher_menor_possivel(valor_a_ser_alcancado)
+
+    def is_empate(self):
+
+        maior_carta_entre_jogadas = [
+            j.jogador
+            for j in self.jogadas_rodada
+            if j.valor_carta == self.maior_carta_rodada.valor_carta
+        ]
+        if len(maior_carta_entre_jogadas) > 1:
+            if self.jogador.parceiro in maior_carta_entre_jogadas:
+                return True
+
+        return False
 
 
 class JogadorProbabilistico(Jogador):
@@ -14,85 +119,37 @@ class JogadorProbabilistico(Jogador):
     def remover_carta_mao(self, carta: Carta) -> None:
         self.mao.remove(carta)
 
-    def definir_estrategia(
-        self, turno: int, maior_carta_rodada: Jogada, pontos_time_rival: int
-    ) -> Carta:
-
-        v_maior_carta = maior_carta_rodada.valor_carta
-        if turno == 4:
-            # print("Escolhendo a menor possivel...")
-            if pontos_time_rival > 1:
-                # tenho que me esforçar pra jogar um pouco maior
-                v_maior_carta += 1
-
-            return self.escolher_menor_possivel(v_maior_carta)
-        else:
-            maior_carta_mao = self.get_maior_carta_mao()
-            if maior_carta_rodada.valor_carta <= maior_carta_mao.num:
-                # print("vou cobrir essa carta com a minha maior")
-                return maior_carta_mao
-            # print("Descarto, pois não consigo cobrir")
-            return self.get_menor_carta_mao()
-
     def escolher_menor_possivel(self, maior_carta_rodada: int) -> Carta:
-        cartas_maiores: List[bool] = []
+        cartas_maiores_ou_iguais: List[bool] = [
+            c.num >= maior_carta_rodada for c in self.mao
+        ]
 
-        for c in self.mao:
-            cartas_maiores.append(c.num >= maior_carta_rodada)
-
-        if any(cartas_maiores):
+        if any(cartas_maiores_ou_iguais):
             # pega a carta suficiente pra ganhar, não necessariamente a maior da mão
-            return self.mao[cartas_maiores.index(True)]
+            return self.mao[cartas_maiores_ou_iguais.index(True)]
         else:
             # nenhuma carta maior ou igual, descarte
             return self.get_menor_carta_mao()
 
-    def escolher_jogada(self, jogadas_rodada: List[Jogada], mesa: Mesa) -> Jogada:
-
-        # o robo sempre tenta vencer, a não ser que o parceiro esteja vencendo...
+    def escolher_jogada(
+        self, jogadas_rodada: List[Jogada], mesa: Mesa, time_inimigo: Time
+    ) -> Jogada:
 
         turno: int = len(jogadas_rodada) + 1
-        if turno == 1:
-            if mesa.jogadores[mesa.jogadores.index(self) - 1].time.pontos_mesa > 0:
-                # mas a minha maior no segundo
-                # print("iniciando com a minha maior")
-                carta_jogada = self.get_maior_carta_mao()
-            else:
-                # print("iniciando com a minha menor")
-                # inicia com a menor carta no primeiro round
-                carta_jogada = self.get_menor_carta_mao()
 
-        else:
-            # mudar para maiores_cart...?
+        maior_carta_rodada = (
+            max(jogadas_rodada, key=lambda x: x.valor_carta)
+            if len(jogadas_rodada) > 0
+            else None
+        )
 
-            v_jogadas_rodada = [j.valor_carta for j in jogadas_rodada]
-            v_maior_carta_rodada = max(v_jogadas_rodada)
-            empate = False
-            if v_jogadas_rodada.count(v_maior_carta_rodada) > 1:
-                empate = True
-
-            if empate:
-                # TODO: se o meu time não fez a primeira rodada, tenho que cobrir!!
-                if jogadas_rodada[-1].jogador.time.pontos_mesa > 0:
-                    # devo cobrir!
-                    # print("Cobrindo o empate")
-                    carta_jogada = self.get_maior_carta_mao()
-                else:
-                    # print(jog;adas_rodada)
-                    # print("Deixo o empate acontecer")
-                    carta_jogada = self.get_menor_carta_mao()
-            else:
-                maior_carta_rodada = max(jogadas_rodada, key=lambda x: x.valor_carta)
-                if maior_carta_rodada.jogador == self.parceiro:
-                    # descarte
-                    # print("Meu parceiro esta fazendo, então...")
-                    carta_jogada = self.get_menor_carta_mao()
-                else:
-                    carta_jogada = self.definir_estrategia(
-                        turno,
-                        maior_carta_rodada,
-                        jogadas_rodada[-1].jogador.time.pontos_mesa,
-                    )
+        carta_jogada = Estrategia(
+            turno,
+            maior_carta_rodada,
+            time_inimigo.pontos_mesa,
+            self,
+            jogadas_rodada,
+        ).escolher_melhor_carta()
 
         self.remover_carta_mao(carta_jogada)
         jogada = Jogada(self, carta_jogada.num)
